11bca7344a5b8039798f3d0bf857a5f314381833
nkd2711 <97438860+nkd2711@users.noreply.github.com>
2022-05-10 00:36:30 +0700

    Add files via upload

---
+++ BaiThucHanh0905/BaiThucHanh0905/src/Slide36_37.java
@@ -0,0 +1,36 @@
+import java.util.Collection;
+import java.util.LinkedList;
+public class Slide36_37 {
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Java");
+        list.add("C++");
+        list.add("PHP");
+        list.add("Java");
+        System.out.println(" Vi du su dung phuong thuc addAll()");
+        System.out.println("-----------------------------------");
+        LinkedList<String> listA = new LinkedList<String>();
+        listA.addAll(list);
+        System.out.print("listA: ");
+        showList(listA);
+        System.out.println("\nvi du su dung phuong thuc retainAll()");
+        System.out.println("-------------------------------------");
+        LinkedList<String> listB = new LinkedList<String>();
+        listB.add("Java");
+        listA.retainAll(listB);
+        System.out.print("listA:");
+        showList(listA);
+        System.out.println("\nvi du su dung phuong thuc removeAll()");
+        System.out.println("---------------------------------------");
+        list.removeAll(listB);
+        System.out.print("list: ");
+        showList(list);
+    }
+    public static void showList(LinkedList<String> list){
+        for (String obj : list){
+            System.out.print("\t" + obj + " , ");
+        }
+        System.out.println();
+
+        }
+    }
